dc4a48f26dccb7cc3ab483ed6ed40172de99880e
phpnajmul <phpnajmul@gmail.com>
2024-02-12 19:14:22 +0600

    I'm create factory & faker data

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index(){
+        return new TaskCollection(Task::paginate());
+    }
+
+    public function show(Request $request, Task $task){
+        return new TaskResource($task);
+    }
+
+    public function store(StoreTaskRequest $request){
+
+        $validated = $request->validated();
+        $task = Task::create($validated);
+        return new TaskResource($task);
+    }
+
+    public function update(UpdateTaskRequest $request, Task $task){
+
+        $validated = $request->validated();
+        $task->update($validated);
+        return new TaskResource($task);
+
+    }
+
+    public function destroy(Task $task){
+        $task->delete();
+        return response()->noContent();
+    }
+
+
+
+
+
+
+
+}
